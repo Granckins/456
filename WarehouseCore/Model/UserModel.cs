0e439c7281dbdde5f302b2bd52a757e175e94e08
Гранкина Ольга <granckina.olga2011@yandex.ua>
2019-03-20 16:24:31 +0300

    first commit

---
+++ WarehouseCore/Model/UserModel.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace WarehouseCore.Model
+{
+    public class UserModel
+    {
+
+        public string UserId { get; set; }
+
+        public string Username { get; set; }
+        public DateTime DateOfJoing { get; set; }
+    public string Password { get; set; }
+    }
+}
